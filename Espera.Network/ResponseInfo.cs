0e4da949c8b0778c15143fc6772521258a6074ab
Dennis Daume <daume.dennis@gmail.com>
2014-02-25 18:57:47 +0100

    Refactor the ResponseInfo to be more strongly typed

--- Espera.Network/ResponseInfo.cs
+++ Espera.Network/ResponseInfo.cs
@@ -1,9 +1,13 @@
-﻿namespace Espera.Network
+﻿using Newtonsoft.Json.Linq;
+
+namespace Espera.Network
 {
     public class ResponseInfo
     {
+        public JObject Content { get; set; }
+
         public string Message { get; set; }
 
-        public int StatusCode { get; set; }
+        public ResponseStatus Status { get; set; }
     }
 }
---
+++ Espera.Network/ResponseStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace Espera.Network
+{
+    public enum ResponseStatus
+    {
+        Success,
+        PlaylistEntryNotFound,
+        Unauthorized,
+        MalformedRequest,
+        NotFound,
+        NotSupported,
+        Rejected
+    }
+}
