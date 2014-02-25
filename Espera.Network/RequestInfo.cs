c3f449291486f9b4cf7abd58073a8316bb90e1be
Dennis Daume <daume.dennis@gmail.com>
2014-02-25 19:07:02 +0100

    Type the request info

---
+++ Espera.Network/RequestInfo.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace Espera.Network
+{
+    public class RequestInfo
+    {
+        public JObject Parameters { get; set; }
+
+        public Guid RequestId { get; set; }
+    }
+}
