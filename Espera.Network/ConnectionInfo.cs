c74bbd398ba9279571601a31d7ca4728798a38e9
Dennis Daume <daume.dennis@gmail.com>
2014-02-26 16:25:56 +0100

    Remove ResponseInfo from ConnectionInfo

--- Espera.Network/ConnectionInfo.cs
+++ Espera.Network/ConnectionInfo.cs
@@ -4,19 +4,13 @@ namespace Espera.Network
 {
     public class ConnectionInfo
     {
-        public ConnectionInfo(NetworkAccessPermission permission, Version serverVersion, ResponseInfo responseInfo)
+        public ConnectionInfo(NetworkAccessPermission permission, Version serverVersion)
         {
-            if (responseInfo == null)
-                throw new ArgumentNullException("responseInfo");
-
             this.AccessPermission = permission;
             this.ServerVersion = serverVersion;
-            this.ResponseInfo = responseInfo;
         }
 
         public NetworkAccessPermission AccessPermission { get; private set; }
-
-        public ResponseInfo ResponseInfo { get; private set; }
 
         public Version ServerVersion { get; private set; }
     }
