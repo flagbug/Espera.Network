6c061e0571f29ab8088d408bcfdd1432a91f14eb
Dennis Daume <daume.dennis@gmail.com>
2014-04-09 02:13:55 +0200

    Add a unit test project

---
+++ Espera.Network.Tests/NetworkHelpersTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Espera.Network.Tests
+{
+    public class NetworkHelpersTest
+    {
+        public class TheReadNextFileTransferMessageAsyncMethod
+        {
+            [Fact]
+            public async Task SmokeTest()
+            {
+                var message = new FileTransferMessage
+                {
+                    Data = new byte[] { 0, 1, 0, 1 },
+                    TransferId = Guid.NewGuid()
+                };
+
+                byte[] packedMessage = await NetworkHelpers.PackFileTransferMessageAsync(message);
+
+                using (var ms = new MemoryStream(packedMessage))
+                {
+                    FileTransferMessage unpackedMessage = await ms.ReadNextFileTransferMessageAsync();
+
+                    Assert.Equal(message.Data, unpackedMessage.Data);
+                    Assert.Equal(message.TransferId, unpackedMessage.TransferId);
+                }
+            }
+        }
+    }
+}
