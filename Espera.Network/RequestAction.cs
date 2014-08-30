7b860a8a1d659a897eaa9c95bf1cebb8f5a531b9
Dennis Daume <daume.dennis@gmail.com>
2014-08-30 12:17:01 +0200

    Added a request action for the guest system info

--- Espera.Network/RequestAction.cs
+++ Espera.Network/RequestAction.cs
@@ -15,6 +15,11 @@
         GetConnectionInfo = 0,
 
         /// <summary>
+        /// Gets the info for the guest system, such as the activation state and the remaining votes.
+        /// </summary>
+        GetGuestSystemInfo = 1,
+
+        /// <summary>
         /// Get the library stored on the server.
         /// </summary>
         GetLibraryContent = 100,
